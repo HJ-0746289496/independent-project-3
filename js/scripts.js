124784b549d3271c55faa655c922bac42962561c
HJ-0746289496 <hamdi469959@gmail.com>
2019-09-27 16:32:58 +0300

    add columns and rows

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready (function(){
+  $(".major").click (function(){
+    $("#img").toggle();
+    $("#para").toggle();
+  });
+  $(".mine").click (function(){
+    $("#dev").toggle();
+    $("#hello").toggle();
+  });
+  $(".min").click (function(){
+    $("#duct").toggle();
+    $("#pro").toggle();
+  });
+  
+});
